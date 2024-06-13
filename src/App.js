c84a692fc092ce6c59430559f499ee422f7d5f70
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-13 21:41:20 +0200

    highlighting even if the call come from direct from the url path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Products from "./components/Products";
@@ -7,32 +7,40 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [currentRoute, setCurrentRoute] = useState("");
+
+  useEffect(() => {
+  const path = window.location.pathname;
+  //console.log(path.slice(1, path.length))
+  setCurrentRoute(path.slice(1, path.length));
+  }, []);
+
   return (
     <BrowserRouter>
       <nav className="m-1 p-1 border-info">
         <ul className="nav nav-pills">
           <li>
             <Link
+              onClick={() => setCurrentRoute("Home")}
               className={
-                currentRoute == "Home"
+                currentRoute === "Home"
                   ? "btn btn-info ms-1"
                   : "btn btn-outline-info ms-1"
               }
               to="/Home"
-              onClick={() => setCurrentRoute('Home')}
             >
               Home
             </Link>
           </li>
           <li>
-            <Link className={
-                currentRoute == "Products"
+            <Link
+              onClick={() => setCurrentRoute("Products")}
+              className={
+                currentRoute === "Products"
                   ? "btn btn-info ms-1"
                   : "btn btn-outline-info ms-1"
               }
               to="/Products"
-              onClick={() => setCurrentRoute('Products')}
-              >
+            >
               Products
             </Link>
           </li>
