40b27dee6274411478299cb29c4bd0673e5be2ad
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-13 20:56:47 +0200

    highlihting the current nav route

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import Products from './components/Products';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Products from "./components/Products";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function App() {
+  const [currentRoute, setCurrentRoute] = useState("");
   return (
-      <BrowserRouter>
-        <nav className='m-1 p-1 border-info'>
-          <ul className='nav nav-pills'>
-            <li>
-              <Link className='btn btn-outline-info ms-1' to='/Home'>Home</Link>
-            </li>
-            <li>
-              <Link className='btn btn-outline-info ms-1' to='/Products'>Products</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Products" element={<Products />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <nav className="m-1 p-1 border-info">
+        <ul className="nav nav-pills">
+          <li>
+            <Link
+              className={
+                currentRoute == "Home"
+                  ? "btn btn-info ms-1"
+                  : "btn btn-outline-info ms-1"
+              }
+              to="/Home"
+              onClick={() => setCurrentRoute('Home')}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={
+                currentRoute == "Products"
+                  ? "btn btn-info ms-1"
+                  : "btn btn-outline-info ms-1"
+              }
+              to="/Products"
+              onClick={() => setCurrentRoute('Products')}
+              >
+              Products
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
