499d4d645c774c22341c2875fa133fe3884eb6ee
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-18 16:48:48 +0200

    search feature init page to 1

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,6 +4,7 @@ import {
   faCheckCircle,
   faCircle,
   faEdit,
+  faSearch,
   faTrash,
 } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -85,73 +86,121 @@ function Products() {
     );
   };
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    handleGetProductsPaginated(
+      paggingState.keyword,
+      1,
+      paggingState.pageSize
+    );
+  };
+
   return (
     <div className="p-1 m-1">
       <div className="row">
-        <div className="col-md-6"></div>
-      </div>
-      <div className="card">
-        <div className="card-header">
-          <h5>Products</h5>
-        </div>
-        <div className="card-body">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Checked</th>
-                <th colSpan="2">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {paggingState.products.map((product) => (
-                <tr key={product.id}>
-                  <td>{product.id}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>
+        <div className="col-md-6">
+          <div className="card mt-1">
+            <div className="card-body">
+              <form onSubmit={(e) => handleSearch(e)}>
+                <div className="row g-2">
+                  <div className="col-auto">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Search..."
+                      value={paggingState.keyword}
+                      onChange={(e) =>
+                        setPaggingState({
+                          ...paggingState,
+                          keyword: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className="col-auto">
                     <button
-                      onClick={() => handleCheckProduct(product.id, product)}
-                      className="btn btn-outline-success"
+                      className="btn btn-success"
+                      onClick={() =>
+                        handleGetProductsPaginated(
+                          paggingState.keyword,
+                          paggingState.currentPage,
+                          paggingState.pageSize
+                        )
+                      }
                     >
-                      <FontAwesomeIcon
-                        icon={product.checked ? faCheckCircle : faCircle}
-                      ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faSearch} />
                     </button>
-                  </td>
-                  <td>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="card mt-1">
+            <div className="card-header">
+              <h5>Products</h5>
+            </div>
+            <div className="card-body">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Checked</th>
+                    <th colSpan="2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {paggingState.products.map((product) => (
+                    <tr key={product.id}>
+                      <td>{product.id}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>
+                        <button
+                          onClick={() =>
+                            handleCheckProduct(product.id, product)
+                          }
+                          className="btn btn-outline-success"
+                        >
+                          <FontAwesomeIcon
+                            icon={product.checked ? faCheckCircle : faCircle}
+                          ></FontAwesomeIcon>
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => handleDeleteProduct(product.id)}
+                          className="btn btn-outline-danger"
+                        >
+                          <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                        </button>
+                        <button className="btn btn-outline-info m-2">
+                          <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <ul className="nav nav-pills">
+                {new Array(paggingState.totalPages).fill(0).map((_, index) => (
+                  <li key={index}>
                     <button
-                      onClick={() => handleDeleteProduct(product.id)}
-                      className="btn btn-outline-danger"
+                      className={
+                        paggingState.currentPage == index + 1
+                          ? "btn btn-info ms-1"
+                          : "btn btn-outline-info ms-1"
+                      }
+                      onClick={() => handleGoToPage(index + 1)}
                     >
-                      <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                      {index + 1}
                     </button>
-                    <button className="btn btn-outline-info m-2">
-                      <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <ul className="nav nav-pills">
-            {new Array(paggingState.totalPages).fill(0).map((_, index) => (
-              <li key={index}>
-                <button
-                  className={
-                    paggingState.currentPage == index + 1
-                      ? "btn btn-info ms-1"
-                      : "btn btn-outline-info ms-1"
-                  }
-                  onClick={() => handleGoToPage(index + 1)}
-                >
-                  {index + 1}
-                </button>
-              </li>
-            ))}
-          </ul>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
